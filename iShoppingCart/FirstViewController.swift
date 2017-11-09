0b19f5eb23c11750bf8fc8c12d8fef5991999685
lenny098 <lennyklwong@hotmail.com>
2017-11-09 23:10:41 +0800

    Initial commit

---
+++ iShoppingCart/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iShoppingCart
+//
+//  Created by Lenny on 9/11/2017.
+//  Copyright © 2017 Group8. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
