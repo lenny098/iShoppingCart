a3d7b35c059520b80caad9098dcbfa98add9a7d6
lenny098 <lennyklwong@hotmail.com>
2017-11-23 12:33:24 +0800

    Updated the DataLoader test

--- iShoppingCartTests/TestDataLoader.swift
+++ iShoppingCartTests/TestDataLoader.swift
@@ -22,14 +22,21 @@ class TestDataLoader: XCTestCase {
     
     func testLoadProduct() {
         let products = DataLoader.loadProducts()
-        // TODO: Replace with a proper check when json is available
-        XCTAssertNotEqual(products, [])
+        
+        let coupon1 = Coupon(name: "Buy 1 get 1 free", productCount: 2, discount: -5.5)
+        let product1 = Product(id: 0, name: "Potato Chips", price: 5.5, section: "Snacks", coupon: coupon1)
+        let product2 = Product(id: 1, name: "Biscuits", price: 7.0, section: "Snacks", coupon: nil)
+        
+        XCTAssertEqual(products, [product1, product2])
     }
     
     func testLoadIBeacons() {
         let iBeacons = DataLoader.loadIBeacons()
-        // TODO: Replace with a proper check when json is available
-        XCTAssertNotEqual(iBeacons, [])
+        
+        let iBeacon1 = IBeacon(uuid: 0, section: "Snacks")
+        let iBeacon2 = IBeacon(uuid: 1, section: "Soft Drinks")
+        
+        XCTAssertEqual(iBeacons, [iBeacon1, iBeacon2])
         
     }
     
