09d1534ec28ac2075b316e328e1cd342bc440aa3
lenny098 <lennyklwong@hotmail.com>
2017-12-25 02:11:37 +0800

    Remove Removal Button
    Changes:
    - Removed Remove Button on List tab
    - Increase the resolution of the QRCode

--- iShoppingCart/domain/QRCodeGenerator.swift
+++ iShoppingCart/domain/QRCodeGenerator.swift
@@ -23,7 +23,7 @@ class QRCodeGenerator
         
         if let filter = CIFilter(name: "CIQRCodeGenerator") {
             filter.setValue(data, forKey: "inputMessage")
-            let transform = CGAffineTransform(scaleX: 5, y: 5)
+            let transform = CGAffineTransform(scaleX: 20, y: 20)
             
             if let output = filter.outputImage?.transformed(by: transform)
             {
